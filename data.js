23db3dd734dce74cff7960f6612a94e77e76f6ae
Pranshoo Rathore <pranshoorathore@gmail.com>
2025-08-03 16:05:21 +0530

    Created GET Apis

---
+++ data.js
@@ -0,0 +1,22 @@
+const products = [
+    {
+        id: 101,
+        name: "iPhone 16",
+        price: 90000,
+        category: "Mobile"
+    },
+    {
+        id: 102,
+        name: "iPad Mini",
+        price: 120000,
+        category: "Tablet"
+    },
+    {
+        id: 103,
+        name: "Macbook",
+        price: 150000,
+        category: "Laptop"
+    }
+];
+
+module.exports = products;
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const products = require('./data.js')
 
 const PORT = 3000;
 const HOST_NAME = 'localhost'; 
 
 const app = express();
+
+app.get('/', (request, response)=>{
+    response.send('Welcome to my Express Server');
+});
+app.get('/products', (request, response)=>{
+    response.send(products);
+});
+app.get('/product/:id', (request, response)=>{
+    const product = products.filter(product=>product.id == request.params.id);
+    response.send(product);
+});
+// app.post();
+// app.put();
+// app.delete();
 
 app.listen(PORT, HOST_NAME, ()=>{
     console.log(`Server started at port: ${PORT} on ${HOST_NAME}`);
