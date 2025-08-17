1333796e7686fd0f980a3029d5efd3bba3d2e7bc
Prashant Bhalerao <bhaleraoprashant48@gmail.com>
2025-08-17 15:28:31 +0530

    CRUD PERFORMED

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const users = require('./Data/users');
 const PORT = 3000;
 const HOST_NAME = "localhost"
 
+
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("WELCOME TO OUR EXPRESS SERVER")
@@ -46,6 +48,69 @@ app.get('/users/:id', (req, res) => {
     const filteredUser = users.filter(user => user.userId == req.params.id);
     res.send(filteredUser);
 })
+
+// POST
+app.post('/products',(req,res)=>{
+    products.push(req.body);
+    res.send('Product Added Successfully');
+})        
+
+// PUT
+app.put('/products/:id',(req,res)=>{
+    for (let i=0;i<products.length;i++){
+        if(products[i].productId==req.params.id){
+            products[i].productId=req.body.productId;
+            products[i].name=req.body.name;
+            products[i].price=req.body.price;
+            products[i].category=req.body.category;
+        }
+    }
+    res.send('Product Updated Successfully')
+});
+
+// DELETE
+app.delete('/products/:id',(req,res)=>{
+    for(let i=0;i<products.length;i++){
+        if(products[i].productId==req.params.id){
+            products.splice(i,1);
+        }
+    }
+    res.send('Product deleted successfully');
+})
+
+
+// USER 
+//POST-CREATE NEW DATABASE
+
+app.post('/users',(req,res)=>{
+    users.push(req.body);
+    res.send('Users Added Successfully');
+})        
+
+
+// PUT-UPDATE THE DATA
+app.put('/users/:id',(req,res)=>{
+    for (let i=0;i<users.length;i++){
+        if(users[i].userId==req.params.id){
+            users[i].userId=req.body.userId;
+            users[i].name=req.body.name;
+            users[i].address=req.body.address;
+            users[i].phone=req.body.phone;
+        }
+    }
+    res.send('User Updated Successfully')
+});
+
+// USER-DELETE
+app.delete('/users/:id',(req,res)=>{
+    for(let i=0;i<users.length;i++){
+        if(users[i].userId==req.params.id){
+            users.splice(i,1);
+        }
+    }
+    res.send('User deleted successfully');
+})
+
 
 app.listen(PORT, HOST_NAME, () => {
     console.log(`Server is running at port:${PORT} at ${HOST_NAME}`)
